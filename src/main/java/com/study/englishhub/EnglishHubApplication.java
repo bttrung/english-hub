42363e56a561471f32806092687cbe280002fc5f
Trung Bui Thanh (Robert) <bttrung.work@gmail.com>
2020-04-11 16:05:43 +0700

    init project

---
+++ src/main/java/com/study/englishhub/EnglishHubApplication.java
@@ -0,0 +1,13 @@
+package com.study.englishhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishHubApplication.class, args);
+	}
+
+}
